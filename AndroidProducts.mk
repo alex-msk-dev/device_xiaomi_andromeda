e720025bdec934e43c24ada9c2c3506e5140055d
alex-msk-dev <baskakoff.aleks2015@yandex.ru>
2020-03-02 17:03:18 +0000

    Andromeda: Switch from add_lunch_combo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_andromeda.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_andromeda-user \
+    lineage_andromeda-userdebug \
+    lineage_andromeda-eng
