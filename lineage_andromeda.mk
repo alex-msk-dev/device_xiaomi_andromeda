08f5bc0e2b463ed4bc5ffad7f420eba921749426
alex-msk-dev <baskakoff.aleks2015@yandex.ru>
2020-03-11 12:17:04 +0000

    Andromeda: update build fingerprint

--- lineage_andromeda.mk
+++ lineage_andromeda.mk
@@ -34,10 +34,10 @@ PRODUCT_MODEL := Mi MIX 3 5G
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/andromeda_eea/andromeda:9/PKQ1.190321.001/V10.3.23.0.PEMEUXM:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="andromeda-user 9 PKQ1.190321.001 V10.3.23.0.PEMEUXM release-keys" \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.021 4565141 release-keys" \
     PRODUCT_NAME="andromeda" \
     TARGET_DEVICE="andromeda"
 
