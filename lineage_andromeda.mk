7b6a8fefd552fe5ca357c1a75669438dce0deda2
alex-msk-dev <baskakoff.aleks2015@yandex.ru>
2020-03-19 09:56:04 +0000

    Andromeda: Update build FP to Coral's Mar 2020 security patch

--- lineage_andromeda.mk
+++ lineage_andromeda.mk
@@ -34,10 +34,10 @@ PRODUCT_MODEL := Mi MIX 3 5G
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.021 4565141 release-keys" \
+    PRIVATE_BUILD_DESC="coral-user 10 QQ2A.200305.003 6156912 release-keys" \
     PRODUCT_NAME="andromeda" \
     TARGET_DEVICE="andromeda"
 
