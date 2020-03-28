4b70b997348a45b7c844445ab4face69f5ab87b6
alex-msk-dev <baskakoff.aleks2015@yandex.ru>
2020-03-28 18:35:40 +0000

    Andromeda: AOSiP bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_andromeda.mk
+    $(LOCAL_DIR)/aosip_andromeda.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_andromeda-user \
-    aosp_andromeda-userdebug
+    aosip_andromeda-user \
+    aosip_andromeda-userdebug
--- aosip_andromeda.mk
+++ aosip_andromeda.mk
@@ -12,16 +12,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from andromeda device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common Pixel Experience stuff.
+# Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="alex.msk"
+
+# GApps
+$(call inherit-product-if-exists, vendor/gapps/config.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_INCLUDE_WIFI_EXT := true
 
+# PixelStyle
+$(call inherit-product-if-exists, vendor/pixelstyle/config.mk)
+
+# Inherit some common Aosip stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_andromeda
+PRODUCT_NAME := aosip_andromeda
 PRODUCT_DEVICE := andromeda
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 3 5G
