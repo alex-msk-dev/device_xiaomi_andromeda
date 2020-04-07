7c64266bb6857db2c57d652b423784abd927c013
alex-msk-dev <baskakoff.aleks2015@yandex.ru>
2020-04-07 11:19:59 +0000

    Andromeda: Support Google Recorder

--- aosp_andromeda.mk
+++ aosp_andromeda.mk
@@ -16,7 +16,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_INCLUDE_WIFI_EXT := true
 
