5b79248fd046cca89ffb36c0c4a28652b1cbd35a
alex-msk-dev <baskakoff.aleks2015@yandex.ru>
2020-04-11 15:51:29 +0000

    Andromeda: Support Google Recorder

--- lineage_andromeda.mk
+++ lineage_andromeda.mk
@@ -22,6 +22,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product-if-exists, vendor/gapps/config.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # PixelStyle
 $(call inherit-product-if-exists, vendor/pixelstyle/config.mk)
