2ed53d5357d7e0f51b4afd1bf000d6235484d43d
alex-msk-dev <baskakoff.aleks2015@yandex.ru>
2020-04-26 09:37:24 +0000

    Andromeda: Make shim Camera
    dlopen failed: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv"
    referenced by "/vendor/lib64/libalRnBRT_GL_GBWRAPPER.so"..., on '/vendor/lib64/camera/components/com.altek.node.distortioncorrection.so'

--- device.mk
+++ device.mk
@@ -97,6 +97,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+# Shims
+PRODUCT_PACKAGES += \
+    com.altek.node.distortioncorrection_shim
+
 # Codec2
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
